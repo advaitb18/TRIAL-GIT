b3d69316167057b1a1ecf78a9d645cdcf7888fad
addy <advait.projects@gmail.com>
2025-05-12 18:16:52 -0700

    Added second file trial2.java

---
+++ LearningGIT/trial2.java
@@ -0,0 +1,12 @@
+package LearningGIT;
+
+public class trial2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a trial for Git.");
+        System.out.println("Let's see how it works.");
+        System.out.println("Adding more lines to test Git.");
+        System.out.println("Final line for this trial.");
+        System.out.println("This is a new line added in trial2.java");
+    }
+}
