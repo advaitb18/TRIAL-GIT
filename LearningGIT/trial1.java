e8d4c341fd7c49475c8db0af75ae0707f1f70d82
addy <advait.projects@gmail.com>
2025-05-12 18:34:05 -0700

    Added trial1 back

---
+++ LearningGIT/trial1.java
@@ -0,0 +1,11 @@
+package LearningGIT;
+
+public class trial1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a trial for Git.");
+        System.out.println("Let's see how it works.");
+        System.out.println("Adding more lines to test Git.");
+        System.out.println("Final line for this trial.");
+    }
+}
